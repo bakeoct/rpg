7668a8ca6e82fba9755254ddef4f1f5c210084d8
= <syunsukerunrun0306@icloud.com>
2024-02-10 16:01:52 +0900

    audioのてすと

--- app/src/main/java/com/example/rpg/graphic/BattleManagerActivity.java
+++ app/src/main/java/com/example/rpg/graphic/BattleManagerActivity.java
@@ -7,6 +7,7 @@ import static com.example.rpg.graphic.GameActivity.monster_cara_now;
 import static com.example.rpg.graphic.TransitionActivity.save_transition_activity;
 import static com.example.rpg.graphic.TransitionActivity.transition_activity;
 
+import android.media.MediaPlayer;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.content.res.Resources;
@@ -32,6 +33,7 @@ import com.example.rpg.Calc.Item.Item;
 import com.example.rpg.Calc.Monsters.Monster2;
 import com.example.rpg.Calc.skill.Skill;
 import com.example.rpg.R;
+import com.example.rpg.sound.MediaPlayerManager;
 
 import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
@@ -45,7 +47,10 @@ public class BattleManagerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle_manager);
-
+        MediaPlayerManager.mediaPlayer.stop();
+        MediaPlayerManager.mediaPlayer.release();
+        MediaPlayerManager.mediaPlayer = MediaPlayer.create(this, R.raw.battlemusic);
+        MediaPlayerManager.mediaPlayer.start();
         int attack_margin = getResources().getDimensionPixelSize(R.dimen.image_margin);
         int my_side_monster_number = 0;
 
--- app/src/main/java/com/example/rpg/graphic/MainActivity.java
+++ app/src/main/java/com/example/rpg/graphic/MainActivity.java
@@ -11,20 +11,24 @@ import android.widget.TextView;
 import static com.example.rpg.Calc.Game.game;
 import static com.example.rpg.graphic.GameActivity.game_activity;
 import static com.example.rpg.graphic.TransitionActivity.transition_activity;
-
+import android.media.MediaPlayer;
 import com.example.rpg.Calc.Game;
 import com.example.rpg.R;
+import com.example.rpg.sound.MediaPlayerManager;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements Serializable {
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button conthinew = findViewById(R.id.conthinew);
-
-        conthinew.setOnClickListener(new View.OnClickListener() {
+        MediaPlayerManager.mediaPlayer = MediaPlayer.create(this, R.raw.bgmusic);
+        MediaPlayerManager.mediaPlayer.start();
+    conthinew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 game.readSave();
@@ -34,5 +38,15 @@ public class MainActivity extends AppCompatActivity implements Serializable {
                 transition_activity = game_activity;
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
